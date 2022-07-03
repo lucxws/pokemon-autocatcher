2a01e8522043529b689abd4f4ae456b002ff2a9a
Lucas <74867419+lucxws@users.noreply.github.com>
2022-07-03 12:08:35 -0300

    read for more information

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+### Main file
 from src.bot.bot import PokémonCatcher
 
 bot = PokémonCatcher(
     token='',
+    ### your account token 
     command_prefix='ac!',
+    ### for now there is not commands
     self_bot=True,
+    ### setting to false it wont work anymore, make sure you are using discord.py 1.4.1 in order to work 
     pokemon_guilds=[],
+    ### add your guild ids to auto catch pokemons. be careful.
     preferred_catch_type='japanese',
+    ### base, chinese, japanese, korean, german, italian, french and spanish
     typing_enabled=True
+    ### typing notification in discord when typing
 )
 
 bot.run()
