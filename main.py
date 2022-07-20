34b3dbf4b2877d1c6df01d7bc6240cef5c4cb83a
Lucas <74867419+lucxws@users.noreply.github.com>
2022-07-20 12:52:35 -0300

    Added system_channel

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ bot = PokemonCatcher(
     ### add your guild ids to auto catch pokemons. be careful.
     preferred_catch_type='japanese',
     ### base, chinese, japanese, korean, german, italian, french and spanish
+    system_channel=123456789011,
+    ### system channel is to get the amount of shadow pokeballs you have, more stuff will be added here later.
     typing_enabled=True
     ### typing notification in discord when typing
 )
